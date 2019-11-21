f453bf6e3435c636ed1eec0e4de1b714507027f2
YutaNozawa <yuta.nozawa.pg@gmail.com>
2019-11-21 22:47:33 +0900

    [add] ヘッダーの追加
    デザインは本当に適当に作っただけです
    背景色はgoogleのシークレットモードの背景色をパクりました

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -2,19 +2,29 @@
 
 <html>
   <head>
-
-    <!--BootstrapのCSS読み込み-->
-    <link rel="stylesheet" type="text/css" href="{{ asset('/css/bootstrap.css') }}">
     <!--jQueryの読み込み-->
-    <script type="text/javascript" src="{{ asset('/js/jquery-3.4.1.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('/js/jquery-3.4.1.js') }}" defer></script>
     <!--Bootstrap.JSの読み込み-->
     <script type="text/javascript" src="{{ asset('/js/bootstrap.js') }}" defer></script>
 
+    <script type="text/javascript" src="{{ asset('/js/jquery-ui.js') }}" defer></script>
+    <!--BootstrapのCSS読み込み-->
+    <link rel="stylesheet" type="text/css" href="{{ asset('/css/bootstrap.css') }}">
     <!--todo.cssの読み込み -->
     <link rel="stylesheet" type="text/css" href="{{ asset('/css/todo.css') }}">
+    <!--header.cssの読み込み -->
+    <link rel="stylesheet" type="text/css" href="{{ asset('/css/header.css') }}">
+
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
     <title>@yield('title')</title>
   </head>
   <body>
+    <header>
+      <p>Todo</p>
+    </header>
     <div class="container">
       @yield('content')
     </div>
