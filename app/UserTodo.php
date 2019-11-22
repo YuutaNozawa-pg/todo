218f27479a303c2cb2d0a87518b3a77d463dab06
YutaNozawa <yuta.nozawa.pg@gmail.com>
2019-11-22 09:41:39 +0900

    [update] Transactionの処理を入れました
    クロージャー内で更新処理をしています。insertからcreateに変えました。セキュリティ上問題があるらしい

--- app/UserTodo.php
+++ app/UserTodo.php
@@ -3,8 +3,14 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class UserTodo extends Model {
+
+  protected $fillable = [
+    'title',
+    'content'
+  ];
 
   //全件取得
   public function getAllData() {
@@ -13,9 +19,11 @@ class UserTodo extends Model {
 
   //1件データを追加
   public function addTitleAndContent($title, $content) {
-    $this->insert([
-      'title' => $title,
-      'content' => $content
-    ]);
+    DB::transaction(function() use ($title, $content) {
+      $this->create([
+          'title' => $title,
+          'content' => $content
+        ]);
+    });
   }
 }
